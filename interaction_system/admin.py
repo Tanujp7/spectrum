6787821021abaab624c06f26d4a2daab0be6cbf1
Suraj Thapar <tunetosuraj@gmail.com>
2017-01-16 12:56:46 +0530

    Added Static Files; Initial Models Created; Activated Django Admin

--- interaction_system/admin.py
+++ interaction_system/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Profile, Publisher, Author, Book
+
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Publisher)
+admin.site.register(Author)
+admin.site.register(Book)

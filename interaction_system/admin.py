ef344b92d06446d7cf225de24286a54f7a9109fc
Suraj Thapar <tunetosuraj@gmail.com>
2017-01-18 15:36:39 +0530

    Added BookProfile with basic properties

--- interaction_system/admin.py
+++ interaction_system/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Location, Publisher, Author, Book, BookUID, BookRating, BookLikeDislike, AuthorLikeDislike
+from .models import Location, Publisher, Author, Book, BookUID, BookRating, BookLikeDislike, AuthorLikeDislike, BookProfile
 
 # Register your models here.
 
@@ -9,6 +9,7 @@ admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(BookUID)
 admin.site.register(BookRating)
+admin.site.register(BookProfile)
 admin.site.register(BookLikeDislike)
 admin.site.register(AuthorLikeDislike)
 admin.site.register(Location)

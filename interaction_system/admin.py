c9d0aa114038708fc98b94435a3a78d0aaae4520
Suraj Thapar <tunetosuraj@gmail.com>
2017-01-18 15:18:07 +0530

    Added Like/Dislike models; Added them to admin controls

--- interaction_system/admin.py
+++ interaction_system/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Location, Publisher, Author, Book, BookUID, BookRating
+from .models import Location, Publisher, Author, Book, BookUID, BookRating, BookLikeDislike, AuthorLikeDislike
 
 # Register your models here.
 
@@ -9,4 +9,6 @@ admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(BookUID)
 admin.site.register(BookRating)
+admin.site.register(BookLikeDislike)
+admin.site.register(AuthorLikeDislike)
 admin.site.register(Location)

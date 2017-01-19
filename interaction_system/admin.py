08072ec5218e02f5723f460251382143e0c00476
Suraj Thapar <tunetosuraj@gmail.com>
2017-01-19 12:31:36 +0530

    Added verbose model names; Added Book Interest Logs;

--- interaction_system/admin.py
+++ interaction_system/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Location, Publisher, Author, Book, BookUID, BookRating, BookLikeDislike, AuthorLikeDislike, BookProfile
+from .models import Location, Publisher, Author, Book, BookUID, BookRating, BookLikeDislike, AuthorLikeDislike, BookProfile, BookInterestLog
 
 # Register your models here.
 
@@ -10,6 +10,7 @@ admin.site.register(Book)
 admin.site.register(BookUID)
 admin.site.register(BookRating)
 admin.site.register(BookProfile)
+admin.site.register(BookInterestLog)
 admin.site.register(BookLikeDislike)
 admin.site.register(AuthorLikeDislike)
 admin.site.register(Location)

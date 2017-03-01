29115aa0a9a8fd20c1e8346492601e012736094f
Suraj Thapar <tunetosuraj@gmail.com>
2017-03-01 12:00:57 +0530

    Config : Static URL

--- violet/settings.py
+++ violet/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_collect')
-STATIC_URL = '/static/'
+STATIC_URL = ':8000/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]

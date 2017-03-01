1b2094d9676f5c7c477514f7a33a71b319c95c7a
Suraj Thapar <tunetosuraj@gmail.com>
2017-03-01 11:18:27 +0530

    Config : domain

--- violet/settings.py
+++ violet/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SPECTRUM_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ['SPECTRUM_DOMAIN1']]
 
 
 # Application definition

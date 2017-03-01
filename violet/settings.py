6896e40e7f153ffa24456df9821bdeabc1605789
Suraj Thapar <tunetosuraj@gmail.com>
2017-03-01 12:04:39 +0530

    Config : Static URL (absolute)

--- violet/settings.py
+++ violet/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_collect')
-STATIC_URL = ':8000/static/'
+STATIC_URL = 'http://survey.protovault.xyz:8000/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]

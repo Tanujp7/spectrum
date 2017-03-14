0fe2c841d54271411b262486a0a4c794ef8fbc38
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-03-14 12:21:33 +0530

    Trying UserProfile class based view

--- interaction_system/urls.py
+++ interaction_system/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import url
 from django.views.generic import RedirectView
 from django.core.urlresolvers import reverse_lazy
 
-from .views import BookRatingList
+from .views import BookRatingList, UserProfile
 
 urlpatterns = [
     url(r'^book_rating/$', BookRatingList.as_view(), name='book_rating_list'),
+    url(r'^about/$', UserProfile.as_view())
 ]
--- interaction_system/views.py
+++ interaction_system/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import BookRating
+from django.views import View
 from vanilla import CreateView, DeleteView, ListView, UpdateView, DetailView, FormView
 
 # Create your views here.
@@ -9,3 +10,9 @@ from vanilla import CreateView, DeleteView, ListView, UpdateView, DetailView, Fo
 class BookRatingList(LoginRequiredMixin, ListView):
     model = BookRating
     paginate_by = 8
+
+class UserProfile(LoginRequiredMixin):
+    greeting = "Good Day!"
+
+    def get(self, request):
+        return HttpResponse(self.greeting)

184deee8d09dbcc1baca098f2d854e24b69702d8
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-03-26 22:08:28 +0530

    Updating templates

--- violet/urls.py
+++ violet/urls.py
@@ -33,4 +33,5 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='allauth/index.html')),
     url(r'^accounts/profile/$', TemplateView.as_view(template_name='allauth/profile.html')),
     url(r'^accounts/email/$', TemplateView.as_view(template_name='allauth/email.html')),
+    url(r'^accounts/login/$', TemplateView.as_view(template_name='allauth/login.html')),
 ]

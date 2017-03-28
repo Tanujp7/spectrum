3670a71e4ab2848bd2c08b0bfab274cdf997890d
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-03-28 12:25:21 +0530

    Purging the classes

--- interaction_system/admin.py
+++ interaction_system/admin.py
@@ -1,16 +1,12 @@
 from django.contrib import admin
 
-from .models import Location, Publisher, Author, Book, BookUID, BookRating, BookLikeDislike, AuthorLikeDislike, BookProfile, BookInterestLog
+from .models import Location, Book, BookRating, BookLikeDislike, BookProfile, BookInterestLog
 
 # Register your models here.
 
-admin.site.register(Publisher)
-admin.site.register(Author)
 admin.site.register(Book)
-admin.site.register(BookUID)
 admin.site.register(BookRating)
 admin.site.register(BookProfile)
 admin.site.register(BookInterestLog)
 admin.site.register(BookLikeDislike)
-admin.site.register(AuthorLikeDislike)
 admin.site.register(Location)

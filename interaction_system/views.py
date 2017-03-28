15345983205b2ecb702579a0a7ca16bc132d92c4
Suraj Thapar <tunetosuraj@gmail.com>
2017-03-28 18:37:53 +0530

    User Profile Form Trial

--- interaction_system/views.py
+++ interaction_system/views.py
@@ -18,7 +18,7 @@ def UserProfileFormView(request):
         user_form = UserForm(request.POST, instance=request.user)
         #location_form = LocationForm(request.POST, instance=request.user.location)
         #qualification_form = QualificationForm(request.POST, instance=request.user.qualification)
-        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        profile_form = ProfileForm(request.POST, instance=request.user.userprofile)
         #if user_form.is_valid() and location_form.is_valid() and qualification_form.is_valid() and profile_form.is_valid():
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
@@ -32,7 +32,7 @@ def UserProfileFormView(request):
         user_form = UserForm(instance=request.user)
         #location_form = LocationForm(request.POST, instance=request.user.location)
         #qualification_form = QualificationForm(request.POST, instance=request.user.qualification)
-        profile_form = ProfileForm(instance=request.user.profile)
+        profile_form = ProfileForm(instance=request.user.userprofile)
     return render(request, 'interaction_system/user_profile.html', {
         'user_form': user_form,
         #'location_form': location_form,

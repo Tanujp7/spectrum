b99cc47a923084c907b49ce26c09500ea1d728a2
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-03-28 14:42:14 +0530

    Catching results on web

--- google_books/views.py
+++ google_books/views.py
@@ -3,17 +3,18 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.views import View
 
+from google_books import googlebooks
+
 class Search(View):
     template_name = 'google_books/search.html'
 
     def get(self, request):
-        results = "Blank"
         try:
             query = request.GET.get('q')
         except:
             query = ''
         if (query != '' and query is not None):
-            results = query
+            results = googlebooks.search(query.replace(" ", "+"))
         else:
             results = None
-        return render(request, self.template_name, {'query': results})
+        return render(request, self.template_name, {'query' : query, 'results' : results})

3a75c699003413ab7ba37b834ffc4fdce5b98609
Suraj Thapar <tunetosuraj@gmail.com>
2017-03-28 18:18:55 +0530

    User Profile Form Trial

--- interaction_system/views.py
+++ interaction_system/views.py
@@ -16,26 +16,27 @@ from .forms import UserForm, LocationForm, QualificationForm, ProfileForm
 def UserProfileFormView(request):
     if request.method == 'POST':
         user_form = UserForm(request.POST, instance=request.user)
-        location_form = LocationForm(request.POST, instance=request.user.location)
-        qualification_form = QualificationForm(request.POST, instance=request.user.qualification)
+        #location_form = LocationForm(request.POST, instance=request.user.location)
+        #qualification_form = QualificationForm(request.POST, instance=request.user.qualification)
         profile_form = ProfileForm(request.POST, instance=request.user.profile)
-        if user_form.is_valid() and location_form.is_valid() and qualification_form.is_valid() and profile_form.is_valid():
+        #if user_form.is_valid() and location_form.is_valid() and qualification_form.is_valid() and profile_form.is_valid():
+        if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
-            location_form.save()
-            qualification_form.save()
+            #location_form.save()
+            #qualification_form.save()
             profile_form.save()
             messages.success(request, _('Your profile was successfully saved!'))
         else:
             messages.error(request, _('Please correct the error below.'))
     else:
         user_form = UserForm(instance=request.user)
-        location_form = LocationForm(request.POST, instance=request.user.location)
-        qualification_form = QualificationForm(request.POST, instance=request.user.qualification)
+        #location_form = LocationForm(request.POST, instance=request.user.location)
+        #qualification_form = QualificationForm(request.POST, instance=request.user.qualification)
         profile_form = ProfileForm(instance=request.user.profile)
     return render(request, 'interaction_system/user_profile.html', {
         'user_form': user_form,
-        'location_form': location_form,
-        'qualification_form': qualification_form,
+        #'location_form': location_form,
+        #'qualification_form': qualification_form,
         'profile_form': profile_form
     })
 

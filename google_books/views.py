e59f404ce43ec37e80781420180c140e045e23e9
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-03-31 02:49:19 +0530

    GB - Form setup w/o Author Publisher

--- google_books/views.py
+++ google_books/views.py
@@ -30,3 +30,25 @@ class AddBook(View):
         else:
             item = None
         return render(request, self.template_name, {'item' : item})
+
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        if form.is_valid():
+            return self.form_valid(form, **kwargs)
+        else:
+            return self.form_invalid(form, **kwargs)
+
+    def form_invalid(self, form, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['form'] = form
+        # here you can add things like:
+        context[show_results] = False
+        return self.render_to_response(context)
+
+    def form_valid(self, form, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['form'] = form
+        # here you can add things like:
+        context[show_results] = True
+        return self.render_to_response(context)

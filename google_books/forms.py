11b19e9c81aa69f9f5eee647c99a514d07013bb6
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-03-30 20:56:25 +0530

    Testing only : 2

--- google_books/forms.py
+++ google_books/forms.py
@@ -5,7 +5,10 @@ from items.models import Book, BookProfile
 class AddBookForm(forms.ModelForm):
     class Meta:
         model = Book
+        exclude = ()
+
 
 class AddBookProfileForm(forms.ModelForm):
     class Meta:
         model = BookProfile
+        exclude = ()

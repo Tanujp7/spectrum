e186d3ddaa8e9e70ec2106a8a027a5fc8dec3b6a
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-03-30 23:25:33 +0530

    Book model updated

--- items/models.py
+++ items/models.py
@@ -6,7 +6,7 @@ from django.core.validators import MinLengthValidator, MaxLengthValidator, MinVa
 
 
 class Book(models.Model):
-    uid = models.CharField(max_length=256)
+    volume_id = models.CharField(max_length=256)
     title = models.CharField(max_length=256)
 
     def __str__(self):
@@ -15,8 +15,8 @@ class Book(models.Model):
 class BookProfile(models.Model):
     book = models.OneToOneField(Book, on_delete=models.CASCADE)
     description = models.CharField(max_length=1024)
-    authors = models.CharField(max_length=1024)
-    publisher = models.CharField(max_length=256)
+    authors = TaggableManager()
+    publisher = TaggableManager()
     publication_year = models.SmallIntegerField(validators=[MinValueValidator(1600), MaxValueValidator(2100)])
     thumbnail = models.CharField(max_length=1024)
     tags = TaggableManager()

3b4027ab9c0f24ab6a1518b9edd6923cea12df13
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-03-31 00:42:29 +0530

    Author, Publisher ae now Models

--- items/models.py
+++ items/models.py
@@ -4,10 +4,23 @@ from taggit.managers import TaggableManager
 
 from django.core.validators import MinLengthValidator, MaxLengthValidator, MinValueValidator, MaxValueValidator
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     volume_id = models.CharField(max_length=256)
     title = models.CharField(max_length=256)
+    authors = models.ManyToManyField(Author)
+    publishers = models.ManyToManyField(Publisher)
 
     def __str__(self):
         return self.title
@@ -15,8 +28,6 @@ class Book(models.Model):
 class BookProfile(models.Model):
     book = models.OneToOneField(Book, on_delete=models.CASCADE)
     description = models.CharField(max_length=1024)
-    authors = models.CharField(max_length=1024)
-    publisher = models.CharField(max_length=256)
     publication_year = models.SmallIntegerField(validators=[MinValueValidator(1600), MaxValueValidator(2100)])
     thumbnail = models.CharField(max_length=1024)
     tags = TaggableManager()

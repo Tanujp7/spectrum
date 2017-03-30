9f4d75d3cbf17f66a9090915c5ed9329c6d78a8c
Suraj Thapar <tunetosuraj@gmail.com>
2017-03-30 11:34:37 +0530

    User Profile Model update

--- people/models.py
+++ people/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Qualification(models.Model):
     qualification_name = models.CharField(max_length=60, null=True)
@@ -36,3 +38,12 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()

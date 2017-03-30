59d73de2ea1392661772dc7b552d26aaf57482ad
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-03-30 18:53:37 +0530

    import method_decorator

--- google_books/views.py
+++ google_books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.decorators import login_required, permission_required
+from django.utils.decorators import method_decorator
 
 from google_books import googlebooks
 

d559462863beee9684ab5639b8db45c9e1a25701
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-03-31 01:37:34 +0530

    Book model complete

--- items/models.py
+++ items/models.py
@@ -1,24 +1,24 @@
 from django.db import models
 
-from taggit.managers import TaggableManager
-
 from django.core.validators import MinLengthValidator, MaxLengthValidator, MinValueValidator, MaxValueValidator
 
+from taggit.managers import TaggableManager
+
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=True, blank=True, default='unknown')
 
     def __str__(self):
         return self.name
 
 class Publisher(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=True, blank=True, default='unknown')
 
     def __str__(self):
         return self.name
 
 class Book(models.Model):
     volume_id = models.CharField(max_length=256)
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=256, null=True, default='unknown')
     authors = models.ManyToManyField(Author)
     publishers = models.ManyToManyField(Publisher)
 
@@ -27,9 +27,15 @@ class Book(models.Model):
 
 class BookProfile(models.Model):
     book = models.OneToOneField(Book, on_delete=models.CASCADE)
-    description = models.CharField(max_length=1024)
-    publication_year = models.SmallIntegerField(validators=[MinValueValidator(1600), MaxValueValidator(2100)])
-    thumbnail = models.CharField(max_length=1024)
+    isbn = models.CharField(max_length=50, null=True, blank=True, default='')
+    description = models.CharField(max_length=1024, null=True, blank=True, default='')
+    publication_date = models.DateField(null=True, blank=True)
+    cover_image_link = models.CharField(max_length=360, null=True, blank=True, default='https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg')
+    page_count = models.PositiveSmallIntegerField(null=True, blank=True, default=0)
+    rating = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], null=True, blank=True, default=0.0)
+    rating_count = models.PositiveIntegerField(null=True, blank=True, default=0)
+    language = models.CharField(max_length=4, null=True, blank=True, default='en')
+    cost = models.CharField(max_length=20, null=True, blank=True, default='0')
     tags = TaggableManager()
 
     class Meta:

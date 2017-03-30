be7c0f0ca4be0ca9d66cca4fccafd07ccacb098e
Suraj Thapar <tunetosuraj@gmail.com>
2017-03-30 13:38:34 +0530

    User Profile Form update

--- people/models.py
+++ people/models.py
@@ -46,6 +46,7 @@ def create_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
         Qualification.objects.create(user=instance)
 
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.userprofile.save()
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+    instance.qualification.save()

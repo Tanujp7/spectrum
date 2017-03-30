b51e761ca073116f5ea4214bd4cd90179c8f42d4
Suraj Thapar <tunetosuraj@gmail.com>
2017-03-30 12:58:49 +0530

    User Profile Form update

--- people/models.py
+++ people/models.py
@@ -5,7 +5,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Qualification(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, default='ankit')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     qualification_name = models.CharField(max_length=60, null=True)
     qualification_stream = models.CharField(max_length=60, null=True)
 

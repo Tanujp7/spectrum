1cabc67993fe7dd5838ac0cfef6c5fbfdc4ecd2d
Suraj Thapar <tunetosuraj@gmail.com>
2017-03-30 16:33:41 +0530

    Career Form

--- uform/views.py
+++ uform/views.py
@@ -15,7 +15,7 @@ def UserProfileFormView(request):
     if request.method == 'POST':
         user_form = UserForm(request.POST, instance=request.user)
         profile_form = ProfileForm(request.POST, instance=request.user.userprofile)
-        if user_form.is_valid() and qualification_form.is_valid() and profile_form.is_valid():
+        if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
     else:

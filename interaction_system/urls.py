f1d1d61d267572883d430c963e8bb234f2f27756
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-04-01 03:38:24 +0530

    random / dry run

--- interaction_system/urls.py
+++ interaction_system/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import rate_the_book
+from .views import rate_the_book, random_book
 
 urlpatterns = [
     url(r'^get/random/book/$', random_book, name='rate_random_book'),

97f8af8677e949ecfc98727a3e0fb00b723b50b3
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-03-31 23:02:19 +0530

    Interact. Sys / Model updated

--- interaction_system/models.py
+++ interaction_system/models.py
@@ -16,8 +16,9 @@ class BookRating(models.Model):
         return (str(self.rating) + '* - ' + str(self.book.title) + ' - rated by - ' + str(self.user))
 
 class RatingLog(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book_rating = models.ForeignKey(BookRating, on_delete=models.CASCADE)
+    rating = models.SmallIntegerField(choices=[(i, i) for i in range(-1, 2)], default=0)
+    time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return (str(102) + '* - ' + str(self.book.title) + ' -  by - ' + str(self.user))
+        return (str(self.rating) + '* - ' + str(self.book_rating.book.title) + ' -  by - ' + str(self.book_rating.user))

5e091b08570489ad15b425b01b2d3fe3842313ba
Suraj Thapar <tunetosuraj@gmail.com>
2017-03-31 13:25:50 +0530

    Books Add Form

--- google_books/views.py
+++ google_books/views.py
@@ -35,7 +35,7 @@ class AddBook(View):
         form_class = self.get_form_class()
         form = self.get_form(form_class)
         if form.is_valid():
-            return self.form_valid(form, **kwargs)
+            return self.form_valid(form)
         else:
             return self.form_invalid(form, **kwargs)
 
@@ -46,9 +46,5 @@ class AddBook(View):
         context[show_results] = False
         return self.render_to_response(context)
 
-    def form_valid(self, form, **kwargs):
-        context = self.get_context_data(**kwargs)
-        context['form'] = form
-        # here you can add things like:
-        context[show_results] = True
-        return self.render_to_response(context)
+    def form_valid(self, form):
+        return HttpResponseRedirect(self.get_success_url())

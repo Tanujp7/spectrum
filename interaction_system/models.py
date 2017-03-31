72cf2dab03c12c6229af7cde872ffe7c474c5507
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-03-31 22:13:39 +0530

    Intract. Sys - Models updated

--- interaction_system/models.py
+++ interaction_system/models.py
@@ -6,7 +6,7 @@ from items.models import Book
 class BookRating(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.SmallIntegerField(choices=[(i, i) for i in range(1, 6)], default=1)
+    rating = models.SmallIntegerField(choices=[(i, i) for i in range(-1, 2)], default=0)
 
     class Meta:
         unique_together = ('user', 'book')
@@ -15,26 +15,9 @@ class BookRating(models.Model):
     def __str__(self):
         return (str(self.rating) + '* - ' + str(self.book.title) + ' - rated by - ' + str(self.user))
 
-class BookLikeDislike(models.Model):
+class RatingLog(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    like = models.SmallIntegerField(choices=[(i, i) for i in range(-1, 2)], default=0)
-
-    class Meta:
-        unique_together = ('user', 'book')
-        verbose_name = 'Book Like/Dislike'
-        verbose_name_plural = 'Book Likes/Dislikes'
 
     def __str__(self):
-        return (str(self.like) + '* - ' + str(self.book.title) + ' - by - ' + str(self.user))
-
-class BookInterestLog(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    interest_score = models.SmallIntegerField(default=0)
-
-    class Meta:
-        verbose_name = 'Book Interest Log'
-
-    def __str__(self):
-        return (str(self.interest_score) + '* - ' + str(self.book.title) + ' - by - ' + str(self.user))
+        return (str(102) + '* - ' + str(self.book.title) + ' -  by - ' + str(self.user))

edf0f7afcc8df7bda9e7f5665e67eabefc873f33
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-03-31 15:06:43 +0530

    form test / author del

--- google_books/views.py
+++ google_books/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.views import View
+from django.views import View, CreateView
 
 from items.models import Book, BookProfile
 from .forms import BookForm

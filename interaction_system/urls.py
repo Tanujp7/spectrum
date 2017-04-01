4a3c027906dafc4c9b1ac8cac46bac909b1a36d4
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-04-01 20:18:30 +0530

    URL regex updated to support volume_id starting with hyphen

--- interaction_system/urls.py
+++ interaction_system/urls.py
@@ -3,5 +3,6 @@ from .views import rate_the_book, random_book
 
 urlpatterns = [
     url(r'^get/random/book/$', random_book, name='rate_random_book'),
-    url(r'^book/rate/(?P<volume>(?:\w+-)+\w+|(\w+))/$', rate_the_book, name='rate_book'),
+    #url(r'^book/rate/(?P<volume>(?:\w+-)+\w+|(\w+))/$', rate_the_book, name='rate_book'),
+    url(r'^book/rate/(?P<volume>([A-Za-z0-9\-\_]+))/$', rate_the_book, name='rate_book'),
 ]

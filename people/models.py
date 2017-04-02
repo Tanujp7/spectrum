2aa636c53a47b0adae2306473f2d4e9a33d5758d
Suraj Thapar <tunetosuraj@gmail.com>
2017-04-02 22:16:48 +0530

    Interest Form

--- people/models.py
+++ people/models.py
@@ -87,7 +87,7 @@ class UserProfile(models.Model):
 
 class Interest(models.Model):
     user = models.ManyToManyField(User)
-    keyword = models.CharField(max_length=256, blank=True, null=True)
+    keyword = models.CharField(max_length=256, blank=True, null=True, unique=True)
 
     def __str__(self):
         return (str(self.keyword))

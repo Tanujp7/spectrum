68eb051ddbccc40a7a6f35d2e982db836ed71b73
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-04-09 16:31:54 +0530

    Success URLs on allauth

--- people/views.py
+++ people/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render
 from allauth.account.views import LoginView, LogoutView, SignupView
 
 class AuthLoginView(LoginView):
+    success_url = '/'
     template_name = 'allauth/login.html'
 
 class AuthLogoutView(LogoutView):
+    success_url = '/'
     template_name = 'allauth/logout.html'
 
 class AuthSignupView(SignupView):
+    success_url = '/'
     template_name = 'allauth/signup.html'

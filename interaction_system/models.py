de264ce3c3ca71d731257757a5dd9268b12f53cd
Meghna Jha <perfectsurpriseisin@gmail.com>
2017-04-16 03:31:52 +0530

    Added almost all models

--- interaction_system/models.py
+++ interaction_system/models.py
@@ -3,11 +3,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from items.models import Book
 
+from django_pandas.managers import DataFrameManager
+
 class BookRating(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.SmallIntegerField(choices=[(i, i) for i in range(0, 2)], default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = DataFrameManager()
 
     class Meta:
         unique_together = ('user', 'book')
--- recommenders/data.py
+++ recommenders/data.py
@@ -1,7 +1,13 @@
+# Models
 from interaction_system.models import BookRating
+from items.models import Book, BookProfile
+from people.models import Career, Hobbies, Interest, PersonalDetails, UserProfile
+from django.contrib.auth.models import User
+
+# Django-pandas
 from django_pandas.io import read_frame
 
-def get_dataframe(model):
-    qs = model.objects.all()
-    df = read_frame(qs)
-    return df
+def dataframe(model):
+    query_string = model.objects.all()
+    data_frame = read_frame(query_string)
+    return data_frame

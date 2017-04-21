17ddd8602ef60ef6036a6a90b6e3ee2da7965599
Suraj Thapar <tunetosuraj@gmail.com>
2017-04-22 03:19:37 +0530

    Column selection

--- recommenders/views.py
+++ recommenders/views.py
@@ -17,7 +17,7 @@ def bookrating_history(request):
     context = {}
     current_user = request.user
 
-    df = dataframe(BookRating.objects.filter(user=current_user), fieldnames=['rating', 'user', 'book'], index='id')
-    context['book_rating_history'], context['count'] = df.to_html(), BookRating.objects.filter(user=current_user).count()
+    df = dataframe(BookRating.objects.filter(user=current_user), fieldnames=['rating', 'book', 'created_at'], index='id')
+    context['book_rating_history'], context['count'] = df, BookRating.objects.filter(user=current_user).count()
 
     return render(request, 'recommenders/bookrating_history.html', context)

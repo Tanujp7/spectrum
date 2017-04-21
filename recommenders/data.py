adf5792ddf71b3ee6dfcf4d4d46cc4e9bba99162
Suraj Thapar <tunetosuraj@gmail.com>
2017-04-22 02:06:02 +0530

    improved data functions

--- recommenders/data.py
+++ recommenders/data.py
@@ -11,12 +11,7 @@ def query_set(model):
     qs = model.objects.all()
     return qs
 
-def simple_dataframe(model):
-    query_set = model.objects.all()
-    data_frame = read_frame(query_set)
-    return data_frame
-
-def custom_dataframe(model,fieldnames=None,index=None):
+def dataframe(model,fieldnames=None,index=None):
     query_set = model.objects.all()
     if fieldnames:
         if index:
@@ -27,5 +22,5 @@ def custom_dataframe(model,fieldnames=None,index=None):
         if index:
             data_frame = query_set.to_dataframe(index=index)
         else:
-            data_frame = query_set.to_dataframe()
+            data_frame = read_frame(query_set)
     return data_frame

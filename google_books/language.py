6cc2d9c3eb5e694d15043e20f8fd50fadf0cbad7
Suraj Thapar <tunetosuraj@gmail.com>
2017-04-25 05:04:41 +0530

    version update

--- google_books/language.py
+++ google_books/language.py
@@ -5,8 +5,8 @@ from google.cloud import language
 
 def request_entity(text):
     language_client = language.Client()
-    document = language_client.document_from_text(text, version="v1beta2")
-    response = document.analyze_entity_sentiment()
+    document = language_client.document_from_text(text)
+    response = document.analyze_entity_sentiment(version="v1beta2")
     return response
 
 text = "Mia has come to live with her Grandma in a land of forests and snow. It isn't at all like her old life in the city, and at first she feels very different from the new children she sees. But when she watches the snow falling around her one night, Mia realises that she is just like one of the snowflakes - unique and perfect in her own way. A beautiful story about new beginnings and making friends by CBeebies presenter Cerrie Burnell."

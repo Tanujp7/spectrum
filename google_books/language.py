7f335f35e7d4d40fef0eabc248033bff98973396
Suraj Thapar <tunetosuraj@gmail.com>
2017-04-25 04:58:29 +0530

    Feature check

--- google_books/language.py
+++ google_books/language.py
@@ -6,9 +6,17 @@ from google.cloud import language
 def request_entity(text):
     language_client = language.Client()
     document = language_client.document_from_text(text)
-    response = document.analyze_entities()
+    response = document.analyze_entity_sentiment()
     return response
 
 text = "Mia has come to live with her Grandma in a land of forests and snow. It isn't at all like her old life in the city, and at first she feels very different from the new children she sees. But when she watches the snow falling around her one night, Mia realises that she is just like one of the snowflakes - unique and perfect in her own way. A beautiful story about new beginnings and making friends by CBeebies presenter Cerrie Burnell."
 
 r = request_entity(text)
+
+for entity in entity_response.entities:
+    print('=' * 20)
+    print('         name: %s' % (entity.name,))
+    print('         type: %s' % (entity.entity_type,))
+    print('     metadata: %s' % (entity.metadata,))
+    print('     salience: %s' % (entity.salience,))
+    print('     salience: %s' % (entity.sentitment,))

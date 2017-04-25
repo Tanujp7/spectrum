3d2edca768bb8216ca3f4ad0ea111fef7fb8e87a
Suraj Thapar <tunetosuraj@gmail.com>
2017-04-26 00:59:12 +0530

    Functions updated.

--- google_books/language.py
+++ google_books/language.py
@@ -15,11 +15,14 @@ def filter_entities(response):
         if entity.salience >= 0.01: filtered_response.append(entity)
     return response.entities if len(filtered_response) == 0 else filtered_response
 
-text = "Mia has come to live with her Grandma in a land of forests and snow. It isn't at all like her old life in the city, and at first she feels very different from the new children she sees. But when she watches the snow falling around her one night, Mia realises that she is just like one of the snowflakes - unique and perfect in her own way. A beautiful story about new beginnings and making friends by CBeebies presenter Cerrie Burnell."
-r = filter_entities(request_entity(text))
-for entity in r:
+def print_entity_details(entity):
     print('=' * 20)
     print('         name: %s' % (entity.name,))
     print('         type: %s' % (entity.entity_type,))
     print('     metadata: %s' % (entity.metadata,))
     print('     salience: %s' % (entity.salience,))
+
+text = "Mia has come to live with her Grandma in a land of forests and snow. It isn't at all like her old life in the city, and at first she feels very different from the new children she sees. But when she watches the snow falling around her one night, Mia realises that she is just like one of the snowflakes - unique and perfect in her own way. A beautiful story about new beginnings and making friends by CBeebies presenter Cerrie Burnell."
+r = filter_entities(request_entity(text))
+for entity in r:
+    print_entity_details(entity)

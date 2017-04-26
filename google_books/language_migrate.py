a84c5b847a119e2563405efcf780f888b2eaa769
Suraj Thapar <tunetosuraj@gmail.com>
2017-04-26 05:33:21 +0530

    Bulk Migration Script added

---
+++ google_books/language_migrate.py
@@ -0,0 +1,25 @@
+"""
+# Note #
+This file is only meant to be run once. To migrate old models (BookProfile) into Natural Language compatible ones.
+"""
+
+from items.models import BookProfile
+from . import language as gnlp
+
+def request_gnlp(description):
+    language_analysis = gnlp.filter_entities(gnlp.request_entity(description))
+    entity_objects = []
+    for entity in language_analysis:
+        entity_objects.append(gnlp.get_or_create_entity(entity))
+    return entity_objects
+
+def bulk_migrate():
+    old_entries = BookProfile.objects.filter(entities=None)
+
+    for book in old_entries:
+        entity_objects = request_gnlp(book.description)
+        if len(entity_objects) > 0:
+            book.entities.add(*entity_objects)
+        book.save()
+        print('Saving ' + book.book + '...')
+    print('Done!')

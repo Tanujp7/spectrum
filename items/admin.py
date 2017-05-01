3af7617fa77e194a17885346a6aa62a04dfe01f0
Suraj Thapar <tunetosuraj@gmail.com>
2017-05-02 01:46:11 +0530

    Making admin Link models verbose

--- items/admin.py
+++ items/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Book, BookProfile
+from .models import Book, BookProfile, Key
 
 # Register your models here.
 admin.site.register(Book)
 admin.site.register(BookProfile)
+admin.site.register(Key)
--- recommenders/models.py
+++ recommenders/models.py
@@ -8,10 +8,6 @@ class Link(models.Model):
     calculated_weight = models.FloatField()
     origin = models.CharField(max_length=512, null=True, blank=True, default=None)
 
-    def __str__(self):
-        if hasattr(self, "item1") and hasattr(self, "item2") and isinstance(self.item1, models.Field) and isinstance(self.item2, models.Field):
-            return (str(self.item1) + ' | ' + str(self.item1) + ' w = ' + self.weight + ' origin : ' + self.origin)
-
     class Meta:
         abstract = True
 
@@ -19,22 +15,45 @@ class KeyToKeyLink(Link):
     item1 = models.ForeignKey(Key, related_name='%(class)s_item1')
     item2 = models.ForeignKey(Key, related_name='%(class)s_item2')
 
+    def __str__(self):
+        return (str(self.item1) + ' | ' + str(self.item1) + ' w = ' + str(self.weight) + ' origin : ' + str(self.origin))
+
+
 class KeyToDocLink(Link):
     item1 = models.ForeignKey(Key)
     item2 = models.ForeignKey(BookProfile)
+
+    def __str__(self):
+        return (str(self.item1) + ' | ' + str(self.item1) + ' w = ' + str(self.weight) + ' origin : ' + str(self.origin))
+
 
 class KeyToUserLink(Link):
     item1 = models.ForeignKey(Key)
     item2 = models.ForeignKey(UserProfile)
 
+    def __str__(self):
+        return (str(self.item1) + ' | ' + str(self.item1) + ' w = ' + str(self.weight) + ' origin : ' + str(self.origin))
+
+
 class DocToDocLink(Link):
     item1 = models.ForeignKey(BookProfile, related_name='%(class)s_item1')
     item2 = models.ForeignKey(BookProfile, related_name='%(class)s_item2')
+
+    def __str__(self):
+        return (str(self.item1) + ' | ' + str(self.item1) + ' w = ' + str(self.weight) + ' origin : ' + str(self.origin))
+
 
 class DocToUserLink(Link):
     item1 = models.ForeignKey(BookProfile)
     item2 = models.ForeignKey(UserProfile)
 
+    def __str__(self):
+        return (str(self.item1) + ' | ' + str(self.item1) + ' w = ' + str(self.weight) + ' origin : ' + str(self.origin))
+
+
 class UserToUserLink(Link):
     item1 = models.ForeignKey(UserProfile, related_name='%(class)s_item1')
     item2 = models.ForeignKey(UserProfile, related_name='%(class)s_item2')
+
+    def __str__(self):
+        return (str(self.item1) + ' | ' + str(self.item1) + ' w = ' + str(self.weight) + ' origin : ' + str(self.origin))

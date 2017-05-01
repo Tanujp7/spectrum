d83c692910537c1841472dad7819611e58416911
Suraj Thapar <tunetosuraj@gmail.com>
2017-05-01 22:58:08 +0530

    Removed abstarct model from admin; Added actual models to admin instead. (Bug Fix)

--- recommenders/admin.py
+++ recommenders/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Link
+from .models import KeyToKeyLink, KeyToDocLink, KeyToUserLink, DocToDocLink, DocToUserLink, UserToUserLink
 
 # Register your models here.
-admin.site.register(Link)
+admin.site.register(KeyToKeyLink)
+admin.site.register(KeyToDocLink)
+admin.site.register(KeyToUserLink)
+admin.site.register(DocToDocLink)
+admin.site.register(DocToUserLink)
+admin.site.register(UserToUserLink)

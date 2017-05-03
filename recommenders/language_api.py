9f8fdef4385f00ac1dc99e27a84c6890ceafed3c
Suraj Thapar <tunetosuraj@gmail.com>
2017-05-04 02:46:51 +0530

    cleaned_code

--- recommenders/language_api.py
+++ recommenders/language_api.py
@@ -2,6 +2,7 @@ from google.cloud import language # For Google Cloud : Natural Language
 import requests # For MeaningCloud
 import os # For Key (inside Environment variable)
 
+from items.models import BookProfile
 from items.models import Key
 from recommenders.models import KeyToDocLink
 
@@ -90,3 +91,38 @@ class MeaningCloudClassifier:
             self.category = []
             for cat in response.get('category_list'):
                 self.category.append(self._get_or_create(cat))
+
+def api_call(document):
+
+    def gnlp(document):
+        if not document.stop_gnlp:
+            print('Calling Google Natural Language API..')
+            entities = GoogleEntity(document=document)
+            entities.analyse()
+            document.stop_gnlp = True
+            document.save()
+
+    def meaningcloud(document):
+        if not document.stop_meaningcloud:
+            print('Calling MeaningCloud Text Classification API..')
+            iptc_label = MeaningCloudClassifier(document=document)
+            iptc_label.analyse()
+            document.stop_meaningcloud = True
+            document.save()
+
+    # Schedule Jobs here..
+    print('Calling attempt...')
+    gnlp(document)
+    meaningcloud(document)
+    print('Calling completed.')
+
+def migration():
+
+    book_list = BookProfile.objects.all()
+
+    for book in book_list:
+        print('Book : ' + str(book.book))
+        api_call(book)
+        print('-- next() --> ? ')
+
+    print('-- All Tasks Completed --')
